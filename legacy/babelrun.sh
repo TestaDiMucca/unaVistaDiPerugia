d97576985cafb0b511eacf1cf6bbe938b80dd82d
Lazy Imposter <jkwok0714@gmail.com>
2024-01-20 23:10:15 -0800

    Deprecate old project
    Consolidate in src

--- legacy/babelrun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo starting Babel
-babel . --out-dir Compiled --watch --presets=es2015,react --ignore=node_modules,Compiled --source-maps inline
--- legacy/index.js
+++
@@ -1,7 +0,0 @@
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-  // Great success! All the File APIs are supported.
-  // alert('detected File API support!');
-  ReactDOM.render(<App />, document.getElementById("App"));
-} else {
-  alert('The File APIs are not fully supported in this browser.');
-}
