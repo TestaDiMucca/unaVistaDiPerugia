cee39eaa1a57bb079a9118055749a78f5401956e
Justin Kwok <jkwok0714@gmail.com>
2024-06-24 00:13:13 -0700

    Add info popover

--- src/src/components/Settings/SettingsModal.tsx
+++ src/src/components/Settings/SettingsModal.tsx
@@ -1,27 +1,72 @@
-import { Input, Text, VStack } from '@chakra-ui/react';
+import {
+  Input,
+  Text,
+  VStack,
+  InputGroup,
+  InputRightAddon,
+  Checkbox,
+} from '@chakra-ui/react';
 import BasicModal, { ModalProps } from '../common/Modal';
 import { ChangeEvent, useCallback } from 'react';
 import useSettingsContext from 'src/hooks/useSettingsContext';
+import SingleAccordion from '../common/SingleAccordion';
+import { OverlaySettings } from 'src/providers/SettingsProvider';
 
 type Props = Omit<ModalProps, 'children' | 'title'>;
 
 export default function SettingsModal(props: Props) {
-  const { advanceTime, setAdvanceTime } = useSettingsContext();
+  const { advanceTime, setAdvanceTime, overlaySettings, setOverlaySettings } =
+    useSettingsContext();
 
   const handleTimingChange = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => setAdvanceTime(+e.target.value),
+    (e: ChangeEvent<HTMLInputElement>) =>
+      setAdvanceTime(+e.target.value * 1000),
     []
+  );
+
+  const handleToggleOverlaySetting = useCallback(
+    (setting: keyof OverlaySettings) => () => {
+      setOverlaySettings({
+        [setting]: !overlaySettings[setting],
+      });
+    },
+    [overlaySettings]
   );
 
   return (
     <BasicModal title="Settings" {...props}>
       <VStack alignItems="flex-start">
         <Text>Slide interval</Text>
-        <Input
-          value={advanceTime}
-          onChange={handleTimingChange}
-          type="number"
-        ></Input>
+        <InputGroup>
+          <Input
+            value={Math.round(advanceTime / 1000)}
+            onChange={handleTimingChange}
+            placeholder="Interval in seconds"
+            type="number"
+          />
+          <InputRightAddon>s</InputRightAddon>
+        </InputGroup>
+        <Text>Display overlay</Text>
+        <Checkbox
+          isChecked={!!overlaySettings.enabled}
+          onChange={handleToggleOverlaySetting('enabled')}
+        >
+          Enabled
+        </Checkbox>
+        <SingleAccordion title="Overlay settings">
+          <Checkbox
+            isChecked={overlaySettings.showFilename}
+            onChange={handleToggleOverlaySetting('showFilename')}
+          >
+            Show file name
+          </Checkbox>
+          <Checkbox
+            isChecked={overlaySettings.showIndex}
+            onChange={handleToggleOverlaySetting('showIndex')}
+          >
+            Show index
+          </Checkbox>
+        </SingleAccordion>
       </VStack>
     </BasicModal>
   );
